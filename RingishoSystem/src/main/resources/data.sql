59fa12a6ae47d1692079629ffd6eeaf975e18877
seki_SuemitsuAtsushi <suemitsu@seki.co.jp>
2018-06-22 14:54:41 +0900

    merge Test

--- RingishoSystem/src/main/resources/data.sql
+++ RingishoSystem/src/main/resources/data.sql
@@ -1,11 +1,11 @@
-create table IF NOT EXISTS employee (
+﻿create table IF NOT EXISTS employee (
     employee_id INTEGER primary key auto_increment,
     department_id INTEGER,
     division_id INTEGER,
     class_id INTEGER,
     employee_no INTEGER,
     employee_name varchar(200),
-    employee_password varchar(200)
+    employee_password varchar(100)
 );
 
 insert into employee (department_id, division_id, class_id, employee_no, employee_name, employee_password) values (1,1,1,1,'テスト太郎','123');
